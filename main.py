ad7b7e412973e1ef12d660a3d2eaffaaea2c25fd
Alexander <alexander.grischancew@yahoo.de>
2020-03-04 19:28:11 +0100

    Code refinement, bug fixes, added persistence

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 
 import requests
 import re
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import telegram.ext
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler
 import logging
 
 
 ORDERS = dict()
+PERSISTENCEFILENAME = "dmOrderStatusBot.dat"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,8 +22,12 @@ logger = logging.getLogger(__name__)
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-    #TODO
+    update.message.reply_text('Welcome to the inoffical telegram bot to keep up to date with your orders '
+                              'at DM-Fotoworld\n Here are the commands:\n'
+                              '/addorder $OrderNumber $StoreNumber\n'
+                              '/removeorder $OrderNumber $StoreNumber\n'
+                              '/listorder\n'
+                              '/getupdate\n')
 
 
 def help(update, context):
@@ -30,126 +35,153 @@ def help(update, context):
     update.message.reply_text('Help!')
     #TODO
 
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
-def checkArgs(context):
-    print("CHECKING ARGS: " + context.args[0] + " " + context.args[1])
-    return re.match("[0-9]{6}",context.args[0]) and re.match("[0-9]{3,5}",context.args[1])
 
-def checkIfInOrders(orderNumber, storeNumber, userID, delete):
-    print("CHECKING IF TUPLE "+orderNumber+ " "+ storeNumber+ " IS ON WATCHLIST FROM USER: "+str(userID))
+def check_args(context):
+    if len(context.args) != 2:
+        print("THERE WERE NO OR NOT ENOUGH ARGS PASSED")
+        return False
+    print("CHECKING ARGS: " + context.args[0] + " " + context.args[1])
+    return re.fullmatch("[0-9]{6}", context.args[0]) and re.fullmatch("[0-9]{3,5}", context.args[1])
+
+
+def check_in_orders(order_number, store_number, user_id, delete):
+    # This not only checks if the combination of order number and store number is on the given user watchlist, but also
+    # can delete the order if delete is true
+    print("CHECKING IF TUPLE " + order_number + " " + store_number + " IS ON WATCHLIST FROM USER: " + str(user_id))
     print("DELETE IS SET: " + str(delete))
-    currentOrders = ORDERS.get(userID)
-    for order in currentOrders:
-        if order[0] == orderNumber and order[1] == storeNumber:
-            print("TUPLE FOUND: "+ order[0] + " " + order[1])
+    current_orders = ORDERS.get(user_id)
+    for order in current_orders:
+        if order[0] == order_number and order[1] == store_number:
+            print("TUPLE FOUND: " + order[0] + " " + order[1])
             if delete:
                 print("TUPLE DELETED")
-                currentOrders.remove(order)
+                current_orders.remove(order)
+                persistence_update()
             return True
     print("TUPLE NOT FOUND")
     return False
 
-def addOrder(update, context):
+
+def add_order(update, context):
     print("EXECUTING ADD ORDER COMMAND")
-    if not checkArgs(context):
+    if not check_args(context):
         print("ARGS NOT OK")
         update.message.reply_text(
-            'The command: ' + update.message.text + " does not match the format: /addorder $order-number $store-number")
+            "The command: " + update.message.text + " does not match the format: /addorder $order-number $store-number")
 
     else:
         print("ARGS OK")
-        global ORDERS
         chat_id = update.effective_chat.id
-        orderNumber = context.args[0]
-        storeNumber = context.args[1]
+        order_number = context.args[0]
+        store_number = context.args[1]
 
-        print("ORDER NUMBER: "+ orderNumber)
-        print("STORE NUMBER: "+ storeNumber)
+        print("ORDER NUMBER: " + order_number)
+        print("STORE NUMBER: " + store_number)
         print("CHAT ID: " + str(chat_id))
 
         if chat_id not in ORDERS:
+            # If the user is new, a empty watchlist is added, avoids further checks if the user exists
             print("CHAT ID NOT IN ORDERS DICT, ADDING")
             ORDERS[chat_id] = []
 
-        currentOrders = ORDERS.get(chat_id)
+        current_orders = ORDERS.get(chat_id)
 
-        if checkIfInOrders(orderNumber,storeNumber,chat_id,False):
+        if check_in_orders(order_number, store_number, chat_id, False):
             print("ORDER NUMBER & STORE NUMBER TUPLE ALREADY ON WATCHLIST")
             update.message.reply_text(
-                'Your order number: ' + orderNumber + ' and store number: ' + storeNumber + ' is already on your watchlist')
+                "Your order number: " + order_number + " and store number: " + store_number + " is already on "
+                                                                                              "your watchlist")
         else:
-            status = getStatus(storeNumber,orderNumber).json().get("summaryStateCode")
+            status = get_status(store_number, order_number).json().get("summaryStateCode")
             print("ORDER STATUS IS: "+status)
             if status == "ERROR":
                 update.message.reply_text(
-                    'Your order number: ' + orderNumber + ' and store number: ' + storeNumber + ' could not be found, maybe it is not registered in the system yet\nstill it was added to your wachlist')
+                    "Your order number: " + order_number + " and store number: " + store_number + " could not be found,"
+                                                                                                  " maybe it is not "
+                                                                                                  "registered in the "
+                                                                                                  "system yet\nstill it"
+                                                                                                  " was added to your "
+                                                                                                  "wachlist")
 
             else:
                 update.message.reply_text(
-                    'Your order number: ' + orderNumber + ' and store number: ' + storeNumber + ' has been added to your watchlist\nYour Order is currently: '+status)
-            currentOrders.append((orderNumber, storeNumber, status))
-        print("ADD ORDER COMMAND FINISHED")
-        print("-----")
+                    "Your order number: " + order_number + " and store number: " + store_number + " has been added to "
+                                                                                                  "your watchlist\nYour"
+                                                                                                  " Order is currently:"
+                                                                                                  " " + status)
+            current_orders.append([order_number, store_number, status])
+            persistence_update()
+    print("ADD ORDER COMMAND FINISHED")
+    print("-----")
 
-def removeOrder(update, context):
+
+def remove_order(update, context):
     print("EXECUTING REMOVE ORDER COMMAND")
-    if not checkArgs(context):
+    if not check_args(context):
         print("ARGS NOT OK")
         update.message.reply_text(
-            'The command: ' + update.message.text + " does not match the format: /addorder $order-number $store-number")
+            "The command: " + update.message.text + " does not match the format: "
+                                                    "/removeorder $order-number $store-number")
     else:
         print("ARGS OK")
-        orderNumber = context.args[0]
-        storeNumber = context.args[1]
+        order_number = context.args[0]
+        store_number = context.args[1]
         chat_id = update.effective_chat.id
-        print("ORDER NUMBER: "+ orderNumber)
-        print("STORE NUMBER: "+ storeNumber)
+        print("ORDER NUMBER: " + order_number)
+        print("STORE NUMBER: " + store_number)
         print("CHAT ID: " + str(chat_id))
-        if checkIfInOrders(orderNumber,storeNumber,chat_id,True):
+        if check_in_orders(order_number, store_number, chat_id, True):
             print("ORDER WAS ON WATCHLIST")
             update.message.reply_text(
-                'Your order number: ' + orderNumber + ' and store number: ' + storeNumber + ' has been removed from your watchlist')
+                "Your order number: " + order_number + " and store number: " + store_number + " has been removed "
+                                                                                              "from your watchlist")
         else:
             print("ORDER NOT ON WATCHLIST")
             update.message.reply_text(
-                'Your order number: ' + orderNumber + ' and store number: ' + storeNumber + 'is not on your watchlist')
+                "Your order number: " + order_number + " and store number: " + store_number + "is not on "
+                                                                                              "your watchlist")
     print("REMOVE ORDER COMMAND FINISHED")
     print("-----")
 
-def getUpdate(update, context):
+
+def get_update(update, context):
     print("EXECUTE GET UPDATE COMMAND")
-    global ORDERS
     chat_id = update.effective_chat.id
     print("CHAT ID: " + str(chat_id))
-    statusMessage = ""
+    status_message = ""
     if chat_id in ORDERS:
         for order in ORDERS.get(chat_id):
-            shopNumber = order[1]
-            orderNumber = order[0]
-            print("GETTING UPDATE FOR ORDER NR: "+ orderNumber + " STORE NUMBER: "+shopNumber)
-            r = getStatus(shopNumber,orderNumber)
-            currentMessage = "Order Nr: " + orderNumber + "\nFrom store Nr: " + shopNumber + "\nStatus:\n" + r.json().get("summaryStateText")
-            statusMessage = statusMessage + "\n \n" + currentMessage
-    if statusMessage == "":
-        statusMessage = "You are currently not watching any orders."
-    update.message.reply_text(statusMessage)
+            store_number = order[1]
+            order_number = order[0]
+            print("GETTING UPDATE FOR ORDER NR: " + order_number + " STORE NUMBER: "+store_number)
+            response = get_status(store_number, order_number)
+            current_message = "Order Nr: " + order_number + "\nFrom store Nr: " + store_number + "\nStatus:\n" \
+                              + response.json().get("summaryStateText")
+            status_message = status_message + "\n \n" + current_message
+    if status_message == "":
+        status_message = "You are currently not watching any orders."
+    update.message.reply_text(status_message)
     print("FINISHED GET UPDATE COMMAND")
     print("-----")
 
-def listOrders(update, context):
+
+def list_orders(update, context):
     print("EXECUTING LIST ORDERS COMMAND")
     chat_id = update.effective_chat.id
-    print("CHAT ID: "+ str(chat_id))
+    print("CHAT ID: " + str(chat_id))
     message = "Your are currently watching:\n"
+    print(str(ORDERS))
     if chat_id in ORDERS:
         for order in ORDERS.get(chat_id):
-            shopNumber = order[1]
-            orderNumber = order[0]
-            print("ORDER NR: "+orderNumber + " STORE NUMBER: "+ shopNumber +" FOUND ON WATCHLIST")
-            message = message + "Order: " + orderNumber + " store: "+shopNumber + "\n"
+            shop_number = order[1]
+            order_number = order[0]
+            print("ORDER NR: " + order_number + " STORE NUMBER: " + shop_number + " FOUND ON WATCHLIST")
+            message = message + "Order: " + order_number + " store: " + shop_number + "\n"
 
     if message == "Your are currently watching:\n":
         print("NO ORDERS ON WATCHLIST")
@@ -159,49 +191,103 @@ def listOrders(update, context):
     print("FINISHED GET UPDATE COMMAND")
     print("-----")
 
+
 def check_status(context: telegram.ext.CallbackContext):
-    print("EXECUTING SHEDULED CHECK STATUS COMMAND")
-    global ORDERS
+    print("EXECUTING SCHEDULED CHECK STATUS COMMAND")
+    persistence_update_needed = False  # if no order have changed there is no need to update the persistence file
     for user in ORDERS:
         print("CHECKING FOR USER: " + str(user))
-        statusMessage = ""
+        status_message = ""
         for order in ORDERS.get(user):
-            currentMessage=""
-            shopNumber = order[1]
-            orderNumber = order[0]
-            orderCurrentStatus = order [2]
-            print("CHECKING ORDER NUMBER: " + orderNumber + " STORE NUMBER: " + shopNumber)
-            print("CURRENT STATUS IS: " + orderCurrentStatus)
-            r = getStatus(shopNumber,orderNumber)
+            print(str(ORDERS.get(user)))
+            current_message = ""
+            store_number = order[1]
+            order_number = order[0]
+            order_current_status = order[2]
+            print("CHECKING ORDER NUMBER: " + order_number + " STORE NUMBER: " + store_number)
+            print("CURRENT STATUS IS: " + order_current_status)
+            r = get_status(store_number, order_number)
             status = r.json().get("summaryStateCode")
-            print("RETRIEVED STATUS IS: " +status)
-            if status != orderCurrentStatus:
-                currentMessage = "Order Nr: " + orderNumber + " from store Nr: " + shopNumber + " has changed to:\n" + r.json().get("summaryStateText")
+            print("RETRIEVED STATUS IS: " + status)
+            if status != order_current_status:
+                persistence_update_needed = True
+                current_message = "Order Nr: " + order_number + " from store Nr: " + store_number + \
+                                  " has changed form " + order_current_status + " to:\n" + \
+                                  r.json().get("summaryStateText") + "\n\n"
+                order[2] = status
+                print("UPDATED STATUS FOR ORDER NR: " + order_number+" FROM STORE NUMBER: " + store_number +
+                      "\nFROM STATUS: " + order_current_status + " --> " + status)
 
-            statusMessage = statusMessage + currentMessage
-        if statusMessage != "":
-            context.bot.send_message(chat_id=user, text=statusMessage)
-    print("FINISHED SHEDULED CHECK STATUS COMMAND")
+            status_message = status_message + current_message
+        if status_message != "":
+            print("SEND MESSAGE TO: " + str(user) + " Message:\n" + status_message)
+            context.bot.send_message(chat_id=user, text=status_message)
+
+    if persistence_update_needed:
+        persistence_update()
+    print("FINISHED SCHEDULED CHECK STATUS COMMAND")
     print("-----")
 
-def getStatus(storeNumber, orderNumber):
-    print("GETTING STATUS FOR ORDER NUMBER: " +orderNumber+" AND STORE NUMBER: " + storeNumber)
+
+def get_status(store_number, order_number):
+    print("GETTING STATUS FOR ORDER NUMBER: " + order_number + " AND STORE NUMBER: " + store_number)
     request_url = "https://spot.photoprintit.com/spotapi/orderInfo/forShop"
     url_parameter = {'config': '1320',
-                     'shop': storeNumber,
-                     'order': orderNumber,
+                     'shop': store_number,
+                     'order': order_number,
                      'language': 'en'}
     print(requests.get(request_url, params=url_parameter).text)
     return requests.get(request_url, params=url_parameter)
 
 
+def persistence_update():
+    print("PERSISTENCE UPDATE TRIGGERED")
+    persistence_file = open(PERSISTENCEFILENAME, "w+")
+    for user in ORDERS:
+        for order in ORDERS.get(user):
+            column = str(user)
+            for data in order:
+                column += " " + data
+            column += "\n"
+            persistence_file.write(column)
+    persistence_file.close()
+    print("PERSISTENCE UPDATE FINISHED")
+
+
+def persistence_load():
+    print("PERSISTENCE LOAD TRIGGERED")
+    if os.path.isfile(PERSISTENCEFILENAME):
+        persistence_file = open(PERSISTENCEFILENAME, "r")
+        persistence_content = persistence_file.read()
+        print("PERSISTENCE DATA FOUND")
+        persistence_content_newline_split = persistence_content.split("\n")
+        for line in persistence_content_newline_split:
+            if line != "":
+                line_split = line.split(" ")
+                print(str(line_split))
+                # it is important that the user id contained in line_split[0] is cast to int else the will be data loss
+                if int(line_split[0]) not in ORDERS:
+                    ORDERS[int(line_split[0])] = []
+                current_user = ORDERS.get(int(line_split[0]))
+                order_list = [line_split[1], line_split[2], line_split[3]]
+                current_user.append(order_list)
+                print(str(ORDERS))
+
+        persistence_file.close()
+    print("PERSISTENCE LOAD FINISHED")
+    print("-----")
+
+
 def main():
     """Start the bot."""
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater(BOTTOKEN, use_context=True)
+    # Create the EventHandler and pass it bot's token.
+    persistence_load()
+    updater = Updater(TOKEN, use_context=True)
 
     jq = updater.job_queue
-    jq.run_repeating(check_status, 21600, first=30)
+    now = 0
+    three_hours = 21600
+    jq.run_repeating(check_status, three_hours, first=now)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -209,12 +295,10 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("addorder", addOrder, pass_args=True))
-    dp.add_handler(CommandHandler("getupdate", getUpdate))
-    dp.add_handler(CommandHandler("listorders", listOrders))
-    dp.add_handler(CommandHandler("removeorder", removeOrder, pass_args=True))
-    # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("addorder", add_order, pass_args=True))
+    dp.add_handler(CommandHandler("getupdate", get_update))
+    dp.add_handler(CommandHandler("listorders", list_orders))
+    dp.add_handler(CommandHandler("removeorder", remove_order, pass_args=True))
 
     # log all errors
     dp.add_error_handler(error)
@@ -222,16 +306,10 @@ def main():
     # Start the Bot
     updater.start_polling()
     print("BOT STARTED")
+    print("-----")
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
